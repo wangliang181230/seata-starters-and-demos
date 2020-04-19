fa1858c8ff80ee63e2cc54bae02454358e601647
王良 <841369634@qq.com>
2020-04-19 09:11:09 +0800

    SAGA例子调整。

--- demo1-seata1.1-springcloudH/applications1/demo1-seata1.1-springcloudH-eureka-feign-mybatisplus3-app1/src/main/java/com/easy/java/demo1/app1/business/service/SagaBizService1.java
+++ demo1-seata1.1-springcloudH/applications1/demo1-seata1.1-springcloudH-eureka-feign-mybatisplus3-app1/src/main/java/com/easy/java/demo1/app1/business/service/SagaBizService1.java
@@ -1,34 +1,26 @@
 package com.easy.java.demo1.app1.business.service;
 
-import com.easy.java.demo1.app1.business.mapper.TestTable1Mapper;
-import com.easy.java.demo1.app1.domain.entity.TestTable1;
+import com.easy.java.demo1.app1.consumer.Demo1Application2AtControllerFeignClient;
 import com.easy.java.starter.seata.util.SeataUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service("sagaBizService1")
 public class SagaBizService1 {
 
 	@Autowired
-	private TestTable1Mapper mapper;
+	private Demo1Application2AtControllerFeignClient atFeignClient;
 
 	public boolean doBiz() throws Throwable {
-		TestTable1 entity = new TestTable1();
-		entity.setName("111");
-		mapper.insert(entity);
-		SeataUtil.print("saga biz service1: insert name=111");
+		atFeignClient.insertAaa(false);
+		SeataUtil.print("saga biz service1: insert name=aaa in application2");
 
 		return true;
 	}
 
 	public boolean compensate() {
-		Map<String, Object> params = new HashMap<>();
-		params.put("name", "111");
-		mapper.deleteByMap(params);
-		SeataUtil.print("saga biz service1: delete name=111");
+		atFeignClient.deleteAaa();
+		SeataUtil.print("saga biz service1: delete name=aaa in application2");
 
 		return true;
 	}
