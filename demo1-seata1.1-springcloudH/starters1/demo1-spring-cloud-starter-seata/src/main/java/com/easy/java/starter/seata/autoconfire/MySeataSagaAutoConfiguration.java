5e45ae389b70e1ee96b989a624e015d0ea3bd429
王良 <841369634@qq.com>
2020-08-21 09:59:56 +0800

    小调整。

--- demo1-seata1.1-springcloudH/starters1/demo1-spring-cloud-starter-seata/src/main/java/com/easy/java/starter/seata/autoconfire/MySeataSagaAutoConfiguration.java
+++ demo1-seata1.1-springcloudH/starters1/demo1-spring-cloud-starter-seata/src/main/java/com/easy/java/starter/seata/autoconfire/MySeataSagaAutoConfiguration.java
@@ -1,6 +1,5 @@
 package com.easy.java.starter.seata.autoconfire;
 
-import com.easy.java.common.util.ResourceUtil;
 import com.easy.java.starter.seata.properties.MySeataSagaThreadPoolProperties;
 import io.seata.saga.engine.StateMachineConfig;
 import io.seata.saga.engine.StateMachineEngine;
@@ -8,21 +7,21 @@ import io.seata.saga.engine.config.DbStateMachineConfig;
 import io.seata.saga.engine.impl.ProcessCtrlStateMachineEngine;
 import io.seata.saga.rm.StateMachineEngineHolder;
 import io.seata.spring.boot.autoconfigure.StarterConstants;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.scheduling.concurrent.ThreadPoolExecutorFactoryBean;
 
 import javax.sql.DataSource;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -32,65 +31,87 @@ import java.util.concurrent.TimeUnit;
  * @author wangliang <841369634@qq.com>
  * @date 2020/4/15
  */
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(StarterConstants.SEATA_PREFIX + ".saga.enabled")
-@AutoConfigureAfter(DataSourceAutoConfiguration.class)
-@EnableConfigurationProperties({MySeataSagaThreadPoolProperties.class})
 public class MySeataSagaAutoConfiguration {
 
-	/**
-	 * Create state machine thread pool bean.
-	 */
-	@Bean
-	public ThreadPoolExecutor sagaStateMachineThreadPoolExecutor(MySeataSagaThreadPoolProperties properties) {
-		ThreadPoolExecutorFactoryBean threadFactory = new ThreadPoolExecutorFactoryBean();
-		threadFactory.setCorePoolSize(properties.getCorePoolSize());
-		threadFactory.setMaxPoolSize(properties.getMaxPoolSize());
-		threadFactory.setKeepAliveSeconds(properties.getKeepAliveTime());
+    public static final String SAGA_ASYNC_THREAD_POOL_EXECUTOR_BEAN_NAME = "seataSagaAsyncThreadPoolExecutor";
+    public static final String SAGA_REJECTED_EXECUTION_HANDLER_BEAN_NAME = "seataSagaRejectedExecutionHandler";
 
-		BlockingQueue<Runnable> queue = new LinkedBlockingQueue();
+    /**
+     * Create state machine config bean.
+     */
+    @Bean
+    @ConditionalOnBean(DataSource.class)
+    @ConditionalOnMissingBean
+    @ConfigurationProperties("seata.saga.state-machine")
+    public StateMachineConfig dbStateMachineConfig(
+            DataSource dataSource,
+            @Autowired(required = false) ThreadPoolExecutor threadPoolExecutor) {
+        DbStateMachineConfig config = new DbStateMachineConfig();
+        config.setDataSource(dataSource);
 
-		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
-				properties.getCorePoolSize(),
-				properties.getMaxPoolSize(),
-				properties.getKeepAliveTime(),
-				TimeUnit.SECONDS,
-				queue,
-				threadFactory
-		);
+        if (threadPoolExecutor != null) {
+            config.setThreadPoolExecutor(threadPoolExecutor);
+        }
 
-		return threadPoolExecutor;
-	}
+        return config;
+    }
 
-	/**
-	 * Create state machine config bean.
-	 */
-	@Bean
-	@ConditionalOnBean(DataSource.class)
-	@ConditionalOnMissingBean
-	@ConfigurationProperties(StarterConstants.SEATA_PREFIX + ".saga.state-machine")
-	public DbStateMachineConfig dbStateMachineConfig(DataSource dataSource, ThreadPoolExecutor threadPoolExecutor) {
-		DbStateMachineConfig config = new DbStateMachineConfig();
-		config.setDataSource(dataSource);
-		config.setThreadPoolExecutor(threadPoolExecutor);
+    /**
+     * Create state machine engine bean.
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public StateMachineEngine stateMachineEngine(StateMachineConfig config) {
+        ProcessCtrlStateMachineEngine engine = new ProcessCtrlStateMachineEngine();
+        engine.setStateMachineConfig(config);
+        new StateMachineEngineHolder().setStateMachineEngine(engine);
+        return engine;
+    }
 
-		Resource[] resources = ResourceUtil.getResources("classpath*:statelang/*.json");
-		if (resources != null && resources.length > 0) {
-			config.setResources(resources);
-		}
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnProperty(name = "seata.saga.enable-async", havingValue = "true")
+    @EnableConfigurationProperties({MySeataSagaThreadPoolProperties.class})
+    static class SagaAsyncThreadPoolExecutorConfiguration {
 
-		return config;
-	}
+        /**
+         * Create rejected execution handler bean.
+         */
+        @Bean(SAGA_REJECTED_EXECUTION_HANDLER_BEAN_NAME)
+        @ConditionalOnMissingBean
+        public RejectedExecutionHandler sagaRejectedExecutionHandler() {
+            return new ThreadPoolExecutor.CallerRunsPolicy();
+        }
 
-	/**
-	 * Create state machine engine bean.
-	 */
-	@Bean
-	@ConditionalOnMissingBean
-	public StateMachineEngine stateMachineEngine(StateMachineConfig config) {
-		ProcessCtrlStateMachineEngine engine = new ProcessCtrlStateMachineEngine();
-		engine.setStateMachineConfig(config);
-		new StateMachineEngineHolder().setStateMachineEngine(engine);
-		return engine;
-	}
+        /**
+         * Create state machine async thread pool executor bean.
+         */
+        @Bean(SAGA_ASYNC_THREAD_POOL_EXECUTOR_BEAN_NAME)
+        @ConditionalOnMissingBean
+        public ThreadPoolExecutor sagaAsyncThreadPoolExecutor(
+                MySeataSagaThreadPoolProperties properties,
+                @Qualifier(SAGA_REJECTED_EXECUTION_HANDLER_BEAN_NAME) RejectedExecutionHandler rejectedExecutionHandler) {
+            ThreadPoolExecutorFactoryBean threadFactory = new ThreadPoolExecutorFactoryBean();
+            threadFactory.setBeanName("sagaStateMachineThreadPoolExecutorFactory");
+            threadFactory.setThreadNamePrefix("sagaAsyncExecute-");
+            threadFactory.setCorePoolSize(properties.getCorePoolSize());
+            threadFactory.setMaxPoolSize(properties.getMaxPoolSize());
+            threadFactory.setKeepAliveSeconds(properties.getKeepAliveTime());
+
+            BlockingQueue<Runnable> queue = new LinkedBlockingQueue();
+
+            ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                    properties.getCorePoolSize(),
+                    properties.getMaxPoolSize(),
+                    properties.getKeepAliveTime(),
+                    TimeUnit.SECONDS,
+                    queue,
+                    threadFactory,
+                    rejectedExecutionHandler
+            );
+
+            return threadPoolExecutor;
+        }
+    }
 }
--- demo1-seata1.1-springcloudH/starters1/demo1-spring-cloud-starter-seata/src/main/java/com/easy/java/starter/seata/util/SeataUtil.java
+++ demo1-seata1.1-springcloudH/starters1/demo1-spring-cloud-starter-seata/src/main/java/com/easy/java/starter/seata/util/SeataUtil.java
@@ -8,16 +8,17 @@ import io.seata.core.context.RootContext;
  */
 public class SeataUtil {
 
-	public static void print(String msg) {
-		long threadId = Thread.currentThread().getId();
-		boolean inGlobalTransaction = RootContext.inGlobalTransaction();
-		String xidType = RootContext.getXIDInterceptorType();
-		String xid = xidType != null ? xidType : RootContext.getXID();
-		System.out.println(String.format("Seata: [%d] [%s] [%s]: %s",
-				threadId,
-				inGlobalTransaction ? "true" : "false",
-				xid,
-				msg));
-	}
+    public static void print(String msg) {
+        long threadId = Thread.currentThread().getId();
+        boolean inGlobalTransaction = RootContext.inGlobalTransaction();
+        String xid = RootContext.getXID();
+        String branchType = RootContext.getBranchType();
+        System.out.println(String.format("Seata: [%d] [%s, %s, %s]: %s",
+                threadId,
+                inGlobalTransaction,
+                xid,
+                branchType,
+                msg));
+    }
 
 }
